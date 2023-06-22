2d37560e2a25155c89a5bbdfc36ebbe305c68a57
Brandon Lyons <brandon_lyons98@yahoo.com>
2023-06-22 13:11:57 -0400

    Add shipFactory tests

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,12 +1,31 @@
 const Ship = require('./shipFactory');
 
-let battleship = new Ship('Battleship', 5);
+// let battleship = new Ship('Battleship', 5);
 
-test('battleship = {5, 5, false}', () => {
-  expect(battleship).toEqual({name: 'Battleship', length: 5, lives: 5, sunk: false})
+beforeEach(function () {
+  battleship = new Ship('Battleship', 5)
 })
 
-test('battleship = {5, 5, false}', () => {
+test('Checks that calling the ship name will return battleship = {5, 5, false}', () => {
+  expect(battleship).toEqual({name: 'Battleship', length: 5, hits: 0, sunk: false});
+})
+
+test('Checks that hit increments to 1', () => {
   battleship.hit();
-  expect(battleship).toEqual({name: 'Battleship', length: 5, lives: 4, sunk: false})
+  expect(battleship.hits).toEqual(1);
+})
+
+test('Checks if battleship is sunk', () => {
+  battleship.setHits(5);
+  battleship.isSunk();
+  expect(battleship.sunk).toEqual(true);
+})
+
+test('getName() returns "Battleship"', () => {
+  expect(battleship.getName()).toEqual('Battleship');
+})
+
+test('Checks that current number of lives accurately updates', () => {
+  battleship.setHits(2);
+  expect(battleship.getLives()).toEqual(3);
 })
